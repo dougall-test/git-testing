aee9eca762fc1856ef1cfc7b98e6b255bb036552
pdougall1 <pdougall1@gmail.com>
2013-02-02 01:29:00 -0500

    fixed the salary to show in dollars

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -16,4 +16,13 @@ class EmployeesController < ApplicationController
 	def show
 		@employee = Employee.find(params[:id])
 	end
+
+	def edit
+		@employee = Employee.find(params[:id])
+		render 'new'	
+	end
+
+	def update
+		@employee = Employee.update(params[:employee])
+	end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,11 @@
 class Employee < ActiveRecord::Base
-  attr_accessible :company_id, :name, :salary
+  attr_accessible :company_id, :name, :salary_in_dollars
+
+ 	def salary_in_dollars
+ 		dollars = salary.to_d/100 if salary
+ 	end 
+
+ 	def salary_in_dollars=(dollars)
+ 		self.salary = dollars.to_d * 100 if dollars.present?
+ 	end
 end
