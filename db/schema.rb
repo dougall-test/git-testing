a45439f2d2b6cffff707dc3759c5e1b40c88e834
pdougall1 <pdougall1@gmail.com>
2013-02-01 19:37:12 -0500

    addinga company resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130122162802) do
+ActiveRecord::Schema.define(:version => 20130202001426) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
     t.integer  "value_range"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "companies", :force => true do |t|
+    t.string   "name"
+    t.integer  "employees"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
